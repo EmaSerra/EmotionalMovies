c31d309cb1e91b81276d945be7aa0034ebd91e3a
EmaSerra <emanuela.serra@icloud.com>
2017-05-18 09:31:55 +0100

    Add files via upload

---
+++ Upload.php
@@ -0,0 +1,28 @@
+
+<?php
+
+if (isset($_FILES['UploadFileField'])){
+    
+    $UploadName=$_FILES['UploadFileField']['name'];
+    $UploadTmp=$_FILES['UploadFileField']['tmp_name'];
+    $UploadType=$_FILES['UploadFileField']['type'];       
+    
+}
+
+$UploadName=  preg_replace("#[^a-z0-9.]#i","", $UploadName);
+
+if(!$UploadTmp){
+die("No file selected. Please Upload again");}
+    else{
+       // move it in the folder uploads
+        move_uploaded_file($UploadTmp, "Uploads/$UploadName");
+         echo "File Uploaded";
+          
+    }
+
+  ?>
+
+<script>
+
+//window.location.replace("index.html");
+</script>
